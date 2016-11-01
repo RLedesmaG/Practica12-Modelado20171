924948f2aba6f287d3ffb604a85ac1ff3375621e
RLedesmaG <r.ledesma@ciencias.unam.mx>
2016-11-01 12:04:57 -0600

    Add files via upload

---
+++ hola_mundo.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+//le dice al compilador que incluya el contenido de stdio (standard input, output).
+
+int main(void){
+	//declaramos la funcion principal del programa
+	printf("¡Hola, mundo!\n");
+	//imprime con salto de linea al final
+	return 0;
+	//si se hace return 0 entonces el programa acabó sin errores.s
+}
